66639fed4a7176cab80b6b640b1863d9de4bee80
Tayfun YÜĞRÜK <tayfunyugruk@gmail.com>
2020-08-23 20:53:15 -0400

    basic http server updated

--- simple-http-server/server.js
+++ simple-http-server/server.js
@@ -1,28 +1,58 @@
 const http = require("http");
 const fs = require("fs");
+const moment = require("moment");
 
-const port = process.env.PORT;
+const port = process.env.PORT || 2222;
 
-const server = http.createServer((req, res) => {
-  
-  if (req.url === "/simple/hello-world") {
+const simpleHandler = function (req, res) {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<h1>Hello, World!</h1>");
+};
+
+const todayDateHandler = function (req, res) {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end(`<h1>Today is ${moment().format("DD/MM/YYYY HH:mm:ss")}</h1>`);
+};
+
+const myProfileHandler = function (req, res) {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.end(`{"name" : "Tayfun"}`);
+};
+
+const noPageFoundHandler = function (res, res) {
+  res.statusCode = 404;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<h1>No page found !</h1>");
+};
+
+const indexHtmlHandler = function (req, res) {
+  fs.readFile("index.html", "utf8", function (err, data) {
+    if (err) {
+      res.statusCode = 500;
+      res.end();
+      return;
+    }
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
-    res.end("<h1>Hello, World!</h1>");
+    res.end(data);
+  });
+};
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  if (req.url === "/simple") {
+    simpleHandler(req, res);
   } else if (req.url === "/index.html") {
-    fs.readFile("index.html", "utf8", function (err, data) {
-      if (err) {
-        res.statusCode = 500;
-        res.end();
-      }
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/html");
-      res.end(data);
-    });
+    indexHtmlHandler(req, res);
+  } else if (req.url === "/today") {
+    todayDateHandler(req, res);
+  } else if (req.url === "/my-profile") {
+    myProfileHandler(req, res);
   } else {
-    res.statusCode = 404;
-    res.setHeader("Content-Type", "text/html");
-    res.end("<h1>Sorry No page found</h1>");
+    noPageFoundHandler(req, res);
   }
 });
 
